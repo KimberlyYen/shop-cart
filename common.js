7e2212de9b1042a2e60b4ddc6a11cc17a38253d6
tomwu <tom511280@gmail.com>
2023-03-08 10:36:43 +0800

    加上業務邏輯

---
+++ common.js
@@ -0,0 +1,75 @@
+//data
+let itemMap = null;
+function initItemMap(){
+    itemMap = {
+        "itemA":{"name":"書", "price":10, "quantity":0, "size": "100ml"},
+        "itemB":{"name":"酒", "price":20, "quantity":0, "size": "200ml"},
+        "itemC":{"name":"外套", "price":15, "quantity":0, "size": "300ml"},
+        "itemD":{"name":"便當", "price":90, "quantity":0, "size": "400ml"},
+        "itemE":{"name":"紅茶", "price":100, "quantity":0, "size": "500ml"},
+        "itemF":{"name":"車子", "price":50, "quantity":0, "size": "600ml"},
+    }
+}
+function setItemMap(inputItemMap){
+    itemMap = inputItemMap
+}
+
+function calculateFunction (id, type, page){
+    let quantity = parseInt(itemMap[id]["quantity"]) 
+    if("add" == type){
+        quantity = quantity + 1;
+        //設定map
+        itemMap[id]['quantity'] = quantity
+        //設定畫面
+        document.getElementById(id+"-quantity").innerHTML = quantity
+    }
+    if("delete" == type){
+        if(quantity > 0) {
+            quantity = quantity - 1;
+            //設定map
+            itemMap[id]['quantity'] = quantity
+            //設定畫面
+            document.getElementById(id+"-quantity").innerHTML = quantity
+            if("confirm" == page && quantity == 0) {
+                document.getElementById(id).style.display="none";
+            }
+        }
+    }
+    //重新加總
+    calculateTotal(page);
+}
+
+function calculateTotal (page) {
+    if("index" == page ){
+        let totalNumber = 0;
+        for (const [key, value] of Object.entries(itemMap)) {
+            const quantity = parseInt(itemMap[`${key}`]["quantity"]);
+            totalNumber = totalNumber + quantity
+        }
+        if(totalNumber > 0){
+            document.getElementById("nextTrue").style.display="block";
+            document.getElementById("nextFalse").style.display="none";
+        }else{
+            document.getElementById("nextTrue").style.display="none";
+            document.getElementById("nextFalse").style.display="block";
+        }
+        document.getElementById("totalNumber").innerHTML = totalNumber; 
+    }
+    if("confirm" == page ){
+        let total = 0;
+        for (const [key, value] of Object.entries(itemMap)) {
+            const quantity = parseInt(itemMap[`${key}`]["quantity"]);
+            const price = parseInt(itemMap[`${key}`]["price"]);
+            total = total + (quantity * price)
+        }
+        if(total > 0){
+            document.getElementById("nextTrue").style.display="block";
+            document.getElementById("nextFalse").style.display="none";
+        }else{
+            document.getElementById("nextTrue").style.display="none";
+            document.getElementById("nextFalse").style.display="block";
+        }
+        document.getElementById("total").innerHTML = total; 
+    }
+    
+}
